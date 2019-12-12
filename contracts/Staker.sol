232ec026f3cd6c454f9200742b903f27f5ad2df9
Michael Kong <michaelkong882@gmail.com>
2019-12-12 16:36:10 +1100

    fix variables in checkDelegatorPendingRewards function

--- contracts/Staker.sol
+++ contracts/Staker.sol
@@ -274,7 +274,7 @@ contract Stakers is StakersConstants {
 
 
     // Returns the pending rewards for a given stakerID
-    function checkPendingRewards (uint256 indexed stakerID, uint256 _untilEpoch, bool isValidator) public {
+    function checkPendingRewards (uint256 indexed stakerID, uint256 _untilEpoch) public {
       uint256 paidUntilEpoch = stakers[stakerID].paidUntilEpoch;
       uint256 pendingRewards = 0;
       for (uint256 e = paidUntilEpoch; e <= untilEpoch; e++) {
@@ -285,11 +285,12 @@ contract Stakers is StakersConstants {
 
 
     // Returns the pending rewards for a given delegator
-    function checkDelegatorPendingRewards (uint256 indexed stakerID, address delegator){
-      uint256 paidUntilEpoch = stakers[stakerID].paidUntilEpoch;
+    function checkDelegatorPendingRewards (uint256 indexed stakerID, uint256 _untilEpoch){
+      uint256 paidUntilEpoch = delegations[msg.sender].paidUntilEpoch;
+      uint256 delegatedAmount = delegations[msg.sender].amount;
       uint256 pendingRewards = 0;
       for (uint256 e = paidUntilEpoch; e <= untilEpoch; e++) {
-        pendingRewards += calcDelegationReward(stakerID, e, delegations[msg.sender].amount);
+        pendingRewards += calcDelegationReward(stakerID, e, delegatedAmount);
       }
       return pendingRewards
     }
