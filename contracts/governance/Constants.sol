bcc1eecfa98b443e233ca781e9729759de07e864
devintegral4 <devintegral4@sfxdx.ru>
2020-04-13 02:37:12 +0300

    added some basic funcs

--- contracts/governance/Constants.sol
+++ contracts/governance/Constants.sol
@@ -6,9 +6,43 @@ import "./SafeMath.sol";
 
 contract Constants {
     using SafeMath for uint256;
-    uint256 constant TYPE_SOFTWARE_UPGRADE = 1; // software upgrade type
-    uint256 constant TYPE_PLAIN_TEXT = 2; // plane text type
-    uint256 constant TYPE_IMMEDIATE_ACTION = 3; // immediate action type
+
+    // types
+    uint256 constant TYPE_SOFTWARE_UPGRADE = 0x1; // software upgrade type
+    uint256 constant TYPE_PLAIN_TEXT = 0x2; // plane text type
+    uint256 constant TYPE_IMMEDIATE_ACTION = 0x3; // immediate action type
+
+    // bit map
+    uint256 constant BIT_IS_ACTIVE = 0;
+    uint256 constant BIT_IS_FROSEN = 1;
+    uint256 constant BIT_IS_VOTING = 2;
+    uint256 constant BIT_IS_ACCEPTED = 3;
+    uint256 constant BIT_IS_FAILED = 4;
+    uint256 constant BIT_IS_IMPLEMENTED = 5;
+
+    // statuses
+    uint256 constant ACTIVE = 1;
+    uint256 constant STATUS_DEPOSITING = ACTIVE;
+    uint256 constant STATUS_DEPOSITING_FAILED = failStatus(STATUS_DEPOSITING);
+    uint256 constant STATUS_VOTING = setStatusVoting(ACTIVE);
+    uint256 constant STATUS_VOTING_FAILED = failStatus(STATUS_VOTING);
+
+    function makeStatusActive(uint256 status) public pure returns (uint256) {
+        status |= 1 << BIT_IS_ACTIVE;
+        return status;
+    }
+
+    function failStatus(uint256 status) public pure returns (uint256) {
+        status &= ~(1 << BIT_IS_ACTIVE);
+        status &= ~(1 << BIT_IS_ACCEPTED);
+        status &= ~(1 << BIT_IS_IMPLEMENTED);
+        return status;
+    }
+
+    function setStatusVoting(uint256 status) public pure returns (uint256) {
+        status |= 1 << BIT_IS_VOTING;
+        return status;
+    }
 
     function typeSoftwareUpgrade() public pure returns (uint256) {
         return TYPE_SOFTWARE_UPGRADE;
@@ -22,5 +56,32 @@ contract Constants {
         return TYPE_IMMEDIATE_ACTION;
     }
 
-    uint256 internal constant PROPOSAL_TYPE_TEXT = 0;
+    // creates special data for a software upgrade proposal
+    function makeSUData(string memory version) internal pure returns (bytes memory) {
+        return bytes(version);
+    }
+
+    function statusActive(uint256 status) internal pure returns (bool) {
+        return (status >> BIT_IS_ACTIVE) & 1 == 1;
+    }
+
+    function statusInactive(uint256 status) internal pure returns (bool) {
+        return (status >> BIT_IS_ACTIVE) & 1 == 0;
+    }
+
+    function statusDepositing(uint256 status) internal pure returns (bool) {
+        return status & STATUS_DEPOSITING == STATUS_DEPOSITING;
+    }
+
+    function statusDepositingFailed(uint256 status) internal pure returns (bool) {
+        return status & STATUS_DEPOSITING_FAILED == STATUS_DEPOSITING_FAILED;
+    }
+
+    function statusActiveVoting(uint256 status) internal pure returns (bool) {
+        return status & STATUS_VOTING == STATUS_VOTING;
+    }
+
+    function statusVotingFailed(uint256 status) internal pure returns (bool) {
+        return status & STATUS_VOTING_FAILED == STATUS_VOTING_FAILED;
+    }
 }
--- contracts/governance/Governance.sol
+++ contracts/governance/Governance.sol
@@ -4,49 +4,139 @@ import "./SafeMath.sol";
 import "./Constants.sol";
 
 
+// add non reentrant!!
 contract Governance is Constants {
     using SafeMath for uint256;
 
-    struct Proposal {
+    struct ProposalDeadlint {
         uint256 id;
-        uint256 propType;
-        uint256 minDeposit;
-        uint256 actualDeposit;
+        uint256 deadline;
+    }
 
+    struct ProposalTimelines {
+        uint256 depositingStartTime;
+        uint256 depositingEndTime;
         uint256 votingStartEpoch;
         uint256 votingEndEpoch;
         uint256 votingStartTime;
         uint256 votingEndTime;
+    }
+
+    struct Proposal {
+        uint256 id;
+        uint256 propType;
+        uint256 status; // status is a bitmask, check out "constants" for a further info
+        uint256 minDeposit;
+        uint256 deposit;
+        ProposalTimelines timelines
 
         string description;
         bytes proposalSpecialData;
     }
 
     uint256 lastProposalId;
+    uint256 earliestActiveEndtime;
+    uint256 earliestPreActiveEndtime;
 
-    // should we set a limit of active and inactive proposals
-    uint256[] activeProposalIds;
-    uint256[] preActiveProposalIds;
+    // TODO: should we set a limit of active and inactive proposals ?
+    uint256[] votingProposalIds;
+    uint256[] depositingProposalIds;
+    uint256[] inactiveProposalIds; // inactive proposal is a proposal that once became active, but was eventualy rejected
+    uint256[] proposalDeadlines;
+    uint256[] deadlines;
 
-    // maps proposal id to an active proposal
-    mapping(uint256 => Proposal) public activeProposals;
-    // maps proposal id to a pre-active proposals. pre active proposal is a proposal which deposit is not filled yet
-    mapping(uint256 => Proposal) public preActiveProposals;
+    address ADMIN;
+
+    mapping(uint256 => uint256) public deadlineMap;
+    mapping(uint256 => Proposal) public proposals;
+    mapping(address => []uint256)) public proposalCreators; // maps proposal id to a voter and its voting power
+    mapping(uint256 => mapping(address => uint256)) public depositors; // maps proposal id to a sender and deposit
+    mapping(uint256 => mapping(address => uint256)) public voters; // maps proposal id to a voter and its voting power
 
     event ProposalIsCreated(uint256 proposalId);
     event ProposalIsResolved(uint256 proposalId);
     event ProposalIsRejected(uint256 proposalId, uint256 reason);
 
+    function maxProposalsPerUser() public view returns (uint256) {
+        return 1;
+    }
+
+    function canCreateProposal(address addr) public returns (bool) {
+        if (addr == ADMIN) {
+            return true;
+        }
+        return proposalCreators[msg.sender].length < maxProposalsPerUser();
+    }
+
+    function ensureProposalCanBeCreated(address addr) public {
+        if (addr == ADMIN) {
+            return;
+        }
+
+        require(proposalCreators[addr].length < maxProposalsPerUser(), "maximum created proposal limit for a user exceeded");
+        require(canCreateProposal(addr), "address has no permissions to create new proposal");
+    }
+
+    function pushNewProposal(Proposal memory prop) {
+        proposals[prop.id] = prop;
+    }
+
     function createSoftwareUpgradeProposal(string memory description, string memory version, uint256 minDeposit) public {
+        ensureProposalCanBeCreated(msg.sender);
         lastProposalId++;
 
         Proposal memory prop;
         prop.id = lastProposalId;
         prop.description = description;
         prop.minDeposit = minDeposit;
-        prop.proposalSpecialData = bytes(version);
+        prop.proposalSpecialData = makeSUData(version);
         prop.propType = typeSoftwareUpgrade();
+        pushNewProposal(prop);
 
         emit ProposalIsCreated(prop.id);
+    }
+
+    function increaseProposalDeposit(uint256 proposalId) public payable {
+        Proposal storage prop = proposals[proposalId];
+
+        require(prop.id != 0, "proposal with a given id doesnt exist");
+        require(statusDepositing(prop.status), "proposal is not depositing");
+        require(msg.value > 0, "msg.value is zero");
+        if (block.timestamp > prop.depositingEndTime) {
+            // deactivateProposal(prop);
+            revert("cannot deposit to an overdue proposal");
+        }
+
+        prop.deposit = prop.deposit.add(msg.value);
+        depositors[prop.id][msg.sender] = depositors[prop.id][msg.sender].add(msg.value);
+    }
+
+    function deactivateProposal(Proposal storage prop) public {
+        bool foundProposal;
+        if (statusDepositing(prop.status)) {
+            deactivateDepositingProposal(prop);
+        }
+    }
+
+    function deactivateDepositingProposal(Proposal storage prop) public {
+        bool foundProposal;
+        for (uint256 i = 0; i < depositingProposalIds.length; i++) {
+            if (depositingProposalIds[i] == prop.id) {
+                foundProposal = true;
+                delete depositingProposalIds[i];
+                break;
+            }
+            if (foundProposal) {
+                depositingProposalIds[i] = depositingProposalIds[i + 1];
+            }
+        }
+
+        if (foundProposal) {
+            revert("proposal is not present in an array");
+        }
+
+        depositingProposalIds[i].length--;
+        prop.status = failStatus(prop.status);
+        inactiveProposalIds.push(prop.id);
     }
 }
