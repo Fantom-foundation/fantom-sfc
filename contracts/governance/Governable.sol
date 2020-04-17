cbcc8440ba61cea30b60ca7f7c2483a65356ff51
devintegral4 <devintegral4@sfxdx.ru>
2020-04-17 12:23:17 +0300

    governable added

---
+++ contracts/governance/Governable.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+interface Governable {
+    function softwareUpgradeTotalVoters() external view returns(uint256);
+    function plainTextTotalVoters() external view returns(uint256);
+    function immediateActionTotalVoters() external view returns(uint256);
+
+    function softwareUpgradeVotingPower(address addr) external view returns(uint256);
+    function plainTextVotingPower(address addr) external view returns(uint256);
+    function immediateActionVotingPower(address addr) external view returns(uint256);
+}
