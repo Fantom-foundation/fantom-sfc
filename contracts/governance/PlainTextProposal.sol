0a9f0fc35c212b2a171a88497eddf2d1caae755b
devintegral4 <devintegral4@sfxdx.ru>
2020-04-23 10:01:43 +0300

    resolving problems with software upgrade proposal

---
+++ contracts/governance/PlainTextProposal.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.0;
+
+import "./SafeMath.sol";
+import "./Constants.sol";
+import "./Governable.sol";
+import "./Upgradability.sol";
+import "./Proposal.sol";
+
+
+contract PlainTextProposalHandler {
+    using SafeMath for uint256;
+}
