d995aa28329e8057aafdcef6997473eaabbddd40
quan8 <quan.nguyen@fantom.foundation>
2020-06-01 20:11:48 +1000

    split tests into new sol file

---
+++ contracts/sfc/UnitTestStakers.sol
@@ -0,0 +1,103 @@
+pragma solidity ^0.5.0;
+
+import "./Staker.sol";
+
+contract TestStakers is Stakers {
+    function stakeLockPeriodTime() public pure returns (uint256) {
+        return 1 * 60;
+    }
+
+    function delegationLockPeriodTime() public pure returns (uint256) {
+        return 1 * 60;
+    }
+}
+
+contract UnitTestStakers is Stakers {
+    uint256[] public stakerIDsArr;
+
+    function _baseRewardPerSecond() public pure returns (uint256) {
+        return 0.0000000001 * 1e18;
+    }
+
+    function minStake() public pure returns (uint256) {
+        return 1 * 1e18;
+    }
+
+    function minStakeIncrease() public pure returns (uint256) {
+        return 1 * 1e18;
+    }
+
+    function minDelegation() public pure returns (uint256) {
+        return 1 * 1e18;
+    }
+
+    constructor (uint256 firstEpoch) public {
+        currentSealedEpoch = firstEpoch;
+    }
+
+    function _markValidationStakeAsCheater(uint256 stakerID, bool cheater) external {
+        if (stakers[stakerID].stakeAmount != 0) {
+            if (cheater) {
+                stakers[stakerID].status = FORK_BIT;
+            } else {
+                stakers[stakerID].status = 0;
+            }
+        }
+    }
+
+    function _createStake() external payable {
+        stakerIDsArr.push(stakersLastID + 1); // SS Check existing?
+        super.createStake("");
+    }
+
+    function _makeEpochSnapshots(uint256 optionalDuration) external returns(uint256) {
+        currentSealedEpoch++;
+        EpochSnapshot storage newSnapshot = epochSnapshots[currentSealedEpoch];
+        uint256 epochPay = 0;
+
+        newSnapshot.endTime = block.timestamp;
+        if (optionalDuration != 0 || currentSealedEpoch == 0) {
+            newSnapshot.duration = optionalDuration;
+        } else {
+            newSnapshot.duration = block.timestamp - epochSnapshots[currentSealedEpoch - 1].endTime;
+        }
+        epochPay += newSnapshot.duration * _baseRewardPerSecond();
+
+        for (uint256 i = 0; i < stakerIDsArr.length; i++) {
+            uint256 deactivatedTime = stakers[stakerIDsArr[i]].deactivatedTime;
+            if (deactivatedTime == 0 || block.timestamp < deactivatedTime) {
+                uint256 basePower = stakers[stakerIDsArr[i]].stakeAmount + stakers[stakerIDsArr[i]].delegatedMe;
+                uint256 txPower = 1000 * i + basePower;
+                newSnapshot.totalBaseRewardWeight += basePower;
+                newSnapshot.totalTxRewardWeight += txPower;
+                newSnapshot.baseRewardPerSecond = _baseRewardPerSecond();
+                newSnapshot.validators[stakerIDsArr[i]] = ValidatorMerit(
+                    stakers[stakerIDsArr[i]].stakeAmount,
+                    stakers[stakerIDsArr[i]].delegatedMe,
+                    basePower,
+                    txPower
+                );
+            }
+        }
+
+        newSnapshot.epochFee = 2 * 1e18;
+        epochPay += newSnapshot.epochFee;
+
+        return epochPay;
+    }
+
+    function discardValidatorRewards() public {
+        uint256 stakerID = _sfcAddressToStakerID(msg.sender);
+        require(stakers[stakerID].stakeAmount != 0, "staker doesn't exist");
+        stakers[stakerID].paidUntilEpoch = currentSealedEpoch;
+    }
+
+    function discardDelegationRewards() public {
+        require(delegations[msg.sender].amount != 0, "delegation doesn't exist");
+        delegations[msg.sender].paidUntilEpoch = currentSealedEpoch;
+    }
+
+    function rewardsAllowed() public view returns (bool) {
+        return true;
+    }
+}
